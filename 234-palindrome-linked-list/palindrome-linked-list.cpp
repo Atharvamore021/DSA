c6d4dadb6efeb4046e2bbb49d483ddce5d73f20a
Atharvamore021 <atharvamore021@gmail.com>
2026-02-22 15:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 121.8 MB (48.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -13,12 +13,10 @@ public:
     ListNode* getMiddle(ListNode* &head){
              ListNode* slow = head;
              ListNode* fast = head->next;
-             while(fast!=NULL){
-                fast = fast->next;
-                if(fast!=NULL){
-                    fast = fast->next;
-                    slow = slow->next;
-                }
+             while(fast!=NULL && fast->next!=NULL){
+                fast = fast->next->next;
+                slow = slow->next;
+               
              }
                 return slow;
         } 
